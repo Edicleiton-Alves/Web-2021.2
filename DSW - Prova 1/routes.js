87876d08831dafd92af5bd34bd02838b868ff1f4
Edicleiton Alves <edicleiton16@gmail.com>
2021-11-22 11:07:51 -0300

    Introdução ao desenvolvimento da SPA :hammer:

---
+++ DSW - Prova 1/routes.js
@@ -0,0 +1,13 @@
+import base from "./views/base64.js";
+import capitalcase from "./views/capitalcase.js";
+import lowercase from "./views/lowercase.js";
+import propercase from "./views/propercase.js";
+import uppercase from "./views/uppercase.js";
+
+export default {
+    base: base(),
+    capitalcase: capitalcase(),
+    lowercase: lowercase(),
+    propercase: propercase(),
+    uppercase: uppercase()
+}
